e380ab18994f07978fda2d0379f87a06f2093c7f
Sameh Hashy <samehhashy@gmail.com>
2024-09-26 18:41:44 +0200

    Add authentication module and related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TaskModule } from './task/task.module';
     }),
     UserModule,
     TaskModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
