201165446eccaa119bc83200790ab43cb24c78dd
Sameh Hashy <samehhashy@gmail.com>
2024-09-26 18:22:58 +0200

    Refactor user controller to use async/await for database operations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -19,30 +19,30 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  getAllUsers(): Promise<User[]> {
-    return this.userService.findAll();
+  async getAllUsers(): Promise<User[]> {
+    return await this.userService.findAll();
   }
 
   @Get(':id')
-  getUser(@Param('id') id: string): Promise<User> {
-    return this.userService.findOne(id);
+  async getUser(@Param('id') id: string): Promise<User> {
+    return await this.userService.findOne(id);
   }
 
   @Post('register')
-  create(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return this.userService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return await this.userService.create(createUserDto);
   }
 
   @Patch(':id')
-  updateUser(
+  async updateUser(
     @Param('id') id: string,
     @Body() updateUserDto: UpdateUserDto,
   ): Promise<User> {
-    return this.userService.update(id, updateUserDto);
+    return await this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  deleteUser(@Param('id') id: string): Promise<User> {
-    return this.userService.delete(id);
+  async deleteUser(@Param('id') id: string): Promise<User> {
+    return await this.userService.delete(id);
   }
 }
